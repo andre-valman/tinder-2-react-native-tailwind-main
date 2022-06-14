fa3965a9fa13e101409dfe0ce5c10174c7892aaa
andre-valman <andre.valmanaiav@gmail.com>
2022-06-14 19:13:31 -0300

    a

--- services/purchase.js
+++ services/purchase.js
@@ -1,4 +1,20 @@
-export const purchased = async (productId) => {};
+import RNIap, { purchaseUpdateListener } from "react-native-iap";
+
+export const purchased = async (productId) => {
+  let isPurchased = false;
+  try {
+    const purchases = await RNIap.getAvailablePurchases();
+    purchases.forEach((purchase) => {
+      if (purchase.productId === productId) {
+        isPurchased = true;
+        return isPurchased;
+      }
+    });
+    return isPurchased;
+  } catch (err) {
+    return false;
+  }
+};
 
 export const requestPurchase = async (productId) => {};
 
